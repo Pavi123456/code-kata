65ac243b15623ccecebf0d1c5fb39ebee2a932de
kamalbalu <37016789+kamalbalu@users.noreply.github.com>
2018-03-05 12:24:55 +0000

    Create factorial1.cpp

---
+++ factorial1.cpp
@@ -0,0 +1,21 @@
+include<iostream.h>
+using namespace std;
+void main()
+{
+	clrscr();
+	int num, i, fact=1;
+	cout<<"Enter a number : ";
+	cin>>num;
+  if(num<=20)
+  {
+	for(i=num; i>0; i--)
+	{
+		fact=fact*i;
+	}
+	cout<<"Factorial of "<<num<<" is "<<fact;
+  }
+  }
+  else
+  cout<<"the output is greater than 20";
+  }
+	
