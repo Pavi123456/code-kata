d0f95b0e67adc23eaac90660d5f1a36fea996812
kamalbalu <37016789+kamalbalu@users.noreply.github.com>
2018-03-10 12:41:05 +0530

    Create rev.c

---
+++ rev.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    int i;
+    char str[30],s;
+    gets(str);
+    strrev[str];
+    for(i=0;i<strlen(str);i++)
+    {
+        s=str[i];
+        if(s=="a"::s=="e"::s=="i"==s=="o"::s=="u");
+        continue;
+        printf("%c",s);
+    }
+    }
