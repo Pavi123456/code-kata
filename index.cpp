e8fdb0a9d884687caae68252c3b64ed3b53c2071
kamalbalu <37016789+kamalbalu@users.noreply.github.com>
2018-05-25 11:33:47 +0530

    Create index.cpp

---
+++ index.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,i;
+int a[n];
+cin>>n;
+for(i=0;i<n;i++)
+{
+cin>>a[i];
+}
+for(i=0;i<n;i++)
+{
+   cout<<i<<" "<<a[i]<<endl;
+   }
+   return 0;
+}
+    
