20a6399c0fea43a2062e58da7071784e462c84c6
kamalbalu <37016789+kamalbalu@users.noreply.github.com>
2018-10-14 13:49:23 +0530

    Create a4.c

---
+++ a4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char a,z,A,Z,n;
+    scanf("%c",&n);
+     if((n>='a')&&(n<='z')||(n>='A')&&(n<='Z'))
+     printf("alphabet");
+     else
+     printf("no");
+  
+    return 0;
+}
