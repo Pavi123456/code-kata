4f3b69d0daae708a023d39f4ec1b21fa89f5c64a
kamalbalu <37016789+kamalbalu@users.noreply.github.com>
2018-10-14 12:57:22 +0530

    Update a2.c

--- a2.c
+++ a2.c
@@ -3,9 +3,13 @@ int main()
 {
     int n;
     scanf("%d",&n);
+    while(n>0)
+    {
     if(n%2==0)
     printf("even");
     else 
     printf("odd");
+    }
+    printf("invalid");
     return 0;
 }
